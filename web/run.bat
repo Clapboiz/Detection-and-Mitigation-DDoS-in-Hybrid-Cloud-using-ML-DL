bf3e815481a2aeafa2bfa5950d262f180f7cdbe7
Clapboiz <phamconglap03@gmail.com>
2023-12-31 22:18:18 +0700

    merge

--- web/run.bat
+++ web/run.bat
@@ -3,4 +3,4 @@ start "Authentication Server Process" cmd /k "npm start"
 
 ping -n 6 127.0.0.1 > nul
 
-start "Server Process" cmd /k "py -3.11 RandomForest.py"
+start "Server Process" cmd /k "py -3.8 RandomForest.py"
